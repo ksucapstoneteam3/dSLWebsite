f0704e5787905bbbde9998926fc27c821fb1b2fa
Phuong Nguyen <57697430+phuongnth151@users.noreply.github.com>
2021-02-11 01:33:06 -0500

    adding navbar with bootstrap

--- dslwebsite/src/App.js
+++ dslwebsite/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Routes } from './Routes';
+import { Navigation } from './components/Navigation';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <Routes />
     </div>
   );
